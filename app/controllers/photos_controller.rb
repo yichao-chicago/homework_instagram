891ffcccfb97eab04142d999463309b17f0be55c
yichao-chicago <funnyjack@outlook.com>
2014-11-07 16:12:20 -0600

    without optional

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,36 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    photo = Photo.find(params[:id])
+    @caption = photo.caption
+    @source = photo.source
+  end
 
+  def new_form
+  end
+
+  def create_row
+    photo = Photo.new
+    photo.caption = params[:the_caption]
+    photo.source = params[:the_source]
+    photo.save
+  end
+
+  def destroy
+    photo = Photo.find(params[:id])
+    photo.destroy
+  end
+
+  def edit_form
+    @photo = Photo.find(params[:id])
+  end
+
+  def update_row
+    photo = Photo.find(params[:id])
+    @caption = photo.caption = params[:the_caption]
+    @source = photo.source = params[:the_source]
+    photo.save
+    render("index")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   # Routes to READ photos
   get("/all_photos", { :controller => "photos", :action => "index" })
   get("/photo_details/:id", { :controller => "photos", :action => "show" })
+  get("/new_photo_form", { :controller => "photos", :action => "new_form" })
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+  get("/edit_photo_form/:id", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
 
 end
